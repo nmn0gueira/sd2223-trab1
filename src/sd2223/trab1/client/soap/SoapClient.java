1b8cee7360e771d2331a332864933e7363429e2f
Nuno <nm.nogueira@campus.fct.unl.pt>
2023-04-20 00:21:04 +0100

    Changed timeouts to 10 seconds

--- src/sd2223/trab1/client/soap/SoapClient.java
+++ src/sd2223/trab1/client/soap/SoapClient.java
@@ -27,8 +27,8 @@ import jakarta.xml.ws.WebServiceException;
 *
 */
 abstract class SoapClient {
-	protected static final int READ_TIMEOUT = 5000;
-	protected static final int CONNECT_TIMEOUT = 5000;
+	protected static final int READ_TIMEOUT = 10000;
+	protected static final int CONNECT_TIMEOUT = 10000;
 
 	protected static final int MAX_RETRIES = 10;
 	protected static final int RETRY_SLEEP = 3000;
